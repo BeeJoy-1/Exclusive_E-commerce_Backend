de2caa216f1a49fec6c6313dc697c89744874b16
BeeJoy-1 <beejoy3000@gmail.com>
2025-04-21 19:50:07 +0600

    added cors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const chalk = require("chalk");
 const AllRoutes = require("./Routes/index.js");
 const cookieParser = require("cookie-parser");
 
 //All Middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5175"], // Your frontend's origin
+    credentials: true, // Allow credentials
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
